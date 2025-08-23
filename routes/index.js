0fbaebecc62d7623524f0e92e673db89fcbd1c8f
Basil Reji <84758142+basil-reji@users.noreply.github.com>
2025-08-23 18:27:15 +0530

    updated: contents, added: new pages

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Reccap 2026',
+    home: true
+  });
+});
+
+router.get('/about', function (req, res, next) {
+  res.render('about',
+    {
+      title: 'About | Reccap 2026',
+      about: true
+    }
+  );
+});
+
+router.get('/venue', function (req, res, next) {
+  res.render('venue',
+    {
+      title: 'Venue| Reccap 2026',
+      venue: true
+    }
+  );
 });
 
 module.exports = router;

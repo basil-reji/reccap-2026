9c6a217024b6a76a1d03de1041e5568062a65116
Viswanatha Kartha V <kartha@ibm.com>
2025-09-05 09:25:42 +0530

    adds more pages and content

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,43 @@ router.get('/team', function (req, res, next) {
   res.render('team',
     {
       title: 'Team | Reccap 2026',
-      venue: true
+      team: true
+    }
+  );
+});
+
+router.get('/call-for-papers', function (req, res, next) {
+  res.render('call-for-papers',
+    {
+      title: 'Call for Papers | Reccap 2026',
+      callForPapers: true
+    }
+  );
+});
+
+router.get('/important-dates', function (req, res, next) {
+  res.render('important-dates',
+    {
+      title: 'Important Dates | Reccap 2026',
+      importantDates: true
+    }
+  );
+});
+
+router.get('/committee', function (req, res, next) {
+  res.render('committee',
+    {
+      title: 'Organizing Committee | Reccap 2026',
+      committee: true
+    }
+  );
+});
+
+router.get('/contact', function (req, res, next) {
+  res.render('contact',
+    {
+      title: 'Contact Us | Reccap 2026',
+      contact: true
     }
   );
 });
